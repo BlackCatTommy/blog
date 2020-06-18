65dae7d7bc8f21d0fb7b2bf85a6db38bbc378831
yangzihan <519973058@qq.com>
2020-06-18 23:00:09 +0800

    '首次提交'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,11 @@
 const themeConfig = require('./config/theme/')
 
 module.exports = {
-  title: "午后南杂",
+  title: "悟",
   description: 'Enjoy when you can, and endure when you must.',
   dest: 'public',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'icon', href: 'z' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
   theme: 'reco',
